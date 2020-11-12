b1cba16487e0b2a379e9b89007cbd82dd325f8aa
danieltao <chaofan.tao@gmail.com>
2020-11-12 01:24:01 -0500

    each agent should own shares

--- agents.py
+++ agents.py
@@ -3,9 +3,10 @@ from market import generate_order
 
 
 class Agent():
-    def __init__(self, invest_type, id, money=10000):
+    def __init__(self, invest_type, id, money=10000, share=10):
         self.type = invest_type
         self.money = money
+        self.shares = share
         self.id = id
 
     @abstractmethod
@@ -14,6 +15,9 @@ class Agent():
 
     def update_money(self, delta):
         self.money += delta
+    
+    def update_share(self, delta):
+        self.shares += delta
 
 class Fool(Agent):
     def __init__(self, id):
@@ -22,9 +26,9 @@ class Fool(Agent):
     def propose(self, prices):
         share = 1
         if prices[-2] < prices[-1]:
-            return generate_order(prices[-1], 'buy', id, share)
+            return generate_order(prices[-1], 'buy', self, share)
         else:
-            return generate_order(prices[-1], 'sell', id, share)
+            return generate_order(prices[-1], 'sell', self, share)
 
 def generate_agent(id):
     return Fool(id)
